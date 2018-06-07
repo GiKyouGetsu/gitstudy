6d4ab2a33cb79024551f2e7744a379a6606ee594
wei41 <wei41@avaya.com>
2018-06-07 16:47:32 +0800

    add junit test folder

---
+++ SpringbootAngular5/src/test/java/com/av/cn/wei/dev/DemoApplicationTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.av.cn.wei.dev;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author wei41
+ *
+ */
+public class DemoApplicationTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link com.av.cn.wei.dev.DemoApplication#configure(org.springframework.boot.builder.SpringApplicationBuilder)}.
+     */
+    @Test
+    public void testConfigureSpringApplicationBuilder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.av.cn.wei.dev.DemoApplication#main(java.lang.String[])}.
+     */
+    @Test
+    public void testMain() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#SpringBootServletInitializer()}.
+     */
+    @Test
+    public void testSpringBootServletInitializer() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#setRegisterErrorPageFilter(boolean)}.
+     */
+    @Test
+    public void testSetRegisterErrorPageFilter() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#onStartup(javax.servlet.ServletContext)}.
+     */
+    @Test
+    public void testOnStartup() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#createRootApplicationContext(javax.servlet.ServletContext)}.
+     */
+    @Test
+    public void testCreateRootApplicationContext() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#createSpringApplicationBuilder()}.
+     */
+    @Test
+    public void testCreateSpringApplicationBuilder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#run(org.springframework.boot.SpringApplication)}.
+     */
+    @Test
+    public void testRun() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.springframework.boot.web.support.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)}.
+     */
+    @Test
+    public void testConfigureSpringApplicationBuilder1() {
+        fail("Not yet implemented");
+    }
+
+}
